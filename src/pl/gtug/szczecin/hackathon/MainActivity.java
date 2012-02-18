12751ecfbfa3c74594d373b20b160b5c37e0dce9
Paweł Stawicki <pawelstawicki@gmail.com>
2012-02-18 13:32:40 +0100

    Simple list view.

--- src/pl/gtug/szczecin/hackathon/MainActivity.java
+++ src/pl/gtug/szczecin/hackathon/MainActivity.java
@@ -3,6 +3,9 @@ package pl.gtug.szczecin.hackathon;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import pl.gtug.szczecin.R;
 
 public class MainActivity extends Activity
@@ -11,12 +14,18 @@ public class MainActivity extends Activity
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //test co
+
+        ListView listView = (ListView) findViewById(R.id.tasksList);
+
+        ListAdapter adapter = new ArrayAdapter<String>(this, R.layout.todoitem, R.id.itemView, new String[] {
+                "asdf", "111", "2222"
+        });
+
+        listView.setAdapter(adapter);
     }
 }
