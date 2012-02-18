32c7c010569b619caf488967d8cbb5eaca1add58
Jakub Chrzanowski <jakub@chrzanowski.info>
2012-02-18 12:56:22 +0100

    log

--- src/pl/gtug/szczecin/hackathon/MainActivity.java
+++ src/pl/gtug/szczecin/hackathon/MainActivity.java
@@ -1,15 +1,20 @@
 package pl.gtug.szczecin.hackathon;
 
-import pl.gtug.szczecin.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import pl.gtug.szczecin.R;
 
 public class MainActivity extends Activity
 {
+    private static final String TAG = MainActivity.class.getCanonicalName();
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        Log.d(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         //test
