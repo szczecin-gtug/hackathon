cb80644a92baf2c22e2e580fe2f36dd64aaeb90d
Paweł Stawicki <pawelstawicki@gmail.com>
2012-02-19 17:37:32 +0100

    R file

--- gen/pl/gtug/szczecin/R.java
+++ gen/pl/gtug/szczecin/R.java
@@ -15,20 +15,21 @@ public final class R {
         public static final int marker_green=0x7f020001;
     }
     public static final class id {
-        public static final int addItemButton=0x7f070006;
-        public static final int cancelButton=0x7f070004;
-        public static final int center_map_btn=0x7f070007;
+        public static final int addItemButton=0x7f070007;
+        public static final int cancelButton=0x7f070005;
+        public static final int center_map_btn=0x7f070008;
         public static final int itemDetailsView=0x7f070000;
         public static final int itemTitle=0x7f070001;
-        public static final int itemView=0x7f070009;
-        public static final int map1=0x7f070008;
-        public static final int menuAbout=0x7f07000c;
-        public static final int menuExit=0x7f07000d;
-        public static final int menuMap=0x7f07000a;
-        public static final int menuPreferences=0x7f07000b;
-        public static final int saveButton=0x7f070003;
-        public static final int setLocationButton=0x7f070002;
-        public static final int tasksList=0x7f070005;
+        public static final int itemView=0x7f07000a;
+        public static final int locationText=0x7f070002;
+        public static final int map1=0x7f070009;
+        public static final int menuAbout=0x7f07000d;
+        public static final int menuExit=0x7f07000e;
+        public static final int menuMap=0x7f07000b;
+        public static final int menuPreferences=0x7f07000c;
+        public static final int saveButton=0x7f070004;
+        public static final int setLocationButton=0x7f070003;
+        public static final int tasksList=0x7f070006;
     }
     public static final class layout {
         public static final int itemdetails=0x7f030000;
